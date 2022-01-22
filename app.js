dde3a3640400e680bc9a06b164e52dd7432e74fd
echo-outlier <yashshivhare@hotmail.com>
2022-01-22 17:51:58 +0530

    second commit

--- app.js
+++ app.js
@@ -2,33 +2,33 @@
 
 // Express is the underlying that atlassian-connect-express uses:
 // https://expressjs.com
-import express from 'express';
+import express from "express";
 
 // https://expressjs.com/en/guide/using-middleware.html
-import bodyParser from 'body-parser';
-import compression from 'compression';
-import cookieParser from 'cookie-parser';
-import errorHandler from 'errorhandler';
-import morgan from 'morgan';
+import bodyParser from "body-parser";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import errorHandler from "errorhandler";
+import morgan from "morgan";
 
 // atlassian-connect-express also provides a middleware
-import ace from 'atlassian-connect-express';
+import ace from "atlassian-connect-express";
 
 // Use Handlebars as view engine:
 // https://npmjs.org/package/express-hbs
 // http://handlebarsjs.com
-import hbs from 'express-hbs';
+import hbs from "express-hbs";
 
 // We also need a few stock Node modules
-import http from 'http';
-import path from 'path';
-import os from 'os';
-import helmet from 'helmet';
-import nocache from 'nocache';
+import http from "http";
+import path from "path";
+import os from "os";
+import helmet from "helmet";
+import nocache from "nocache";
 
 // Routes live here; this is the C in MVC
-import routes from './routes';
-import { addServerSideRendering } from './server-side-rendering';
+import routes from "./routes";
+import { addServerSideRendering } from "./server-side-rendering";
 
 // Bootstrap Express and atlassian-connect-express
 const app = express();
@@ -36,18 +36,18 @@ const addon = ace(app);
 
 // See config.json
 const port = addon.config.port();
-app.set('port', port);
+app.set("port", port);
 
 // Log requests, using an appropriate formatter by env
-const devEnv = app.get('env') === 'development';
-app.use(morgan(devEnv ? 'dev' : 'combined'));
+const devEnv = app.get("env") === "development";
+app.use(morgan(devEnv ? "dev" : "combined"));
 
 // Configure Handlebars
-const viewsDir = path.join(__dirname, 'views');
-const handlebarsEngine = hbs.express4({partialsDir: viewsDir});
-app.engine('hbs', handlebarsEngine);
-app.set('view engine', 'hbs');
-app.set('views', viewsDir);
+const viewsDir = path.join(__dirname, "views");
+const handlebarsEngine = hbs.express4({ partialsDir: viewsDir });
+app.engine("hbs", handlebarsEngine);
+app.set("view engine", "hbs");
+app.set("views", viewsDir);
 
 // Configure jsx (jsx files should go in views/ and export the root component as the default export)
 addServerSideRendering(app, handlebarsEngine);
@@ -55,17 +55,21 @@ addServerSideRendering(app, handlebarsEngine);
 // Atlassian security policy requirements
 // http://go.atlassian.com/security-requirements-for-cloud-apps
 // HSTS must be enabled with a minimum age of at least one year
-app.use(helmet.hsts({
-  maxAge: 31536000,
-  includeSubDomains: false
-}));
-app.use(helmet.referrerPolicy({
-  policy: ['origin']
-}));
+app.use(
+  helmet.hsts({
+    maxAge: 31536000,
+    includeSubDomains: false,
+  })
+);
+app.use(
+  helmet.referrerPolicy({
+    policy: ["origin"],
+  })
+);
 
 // Include request parsers
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Gzip responses when appropriate
@@ -75,7 +79,7 @@ app.use(compression());
 app.use(addon.middleware());
 
 // Mount the static files directory
-const staticDir = path.join(__dirname, 'public');
+const staticDir = path.join(__dirname, "public");
 app.use(express.static(staticDir));
 
 // Atlassian security policy requirements
@@ -89,8 +93,10 @@ if (devEnv) app.use(errorHandler());
 routes(app, addon);
 
 // Boot the HTTP server
-http.createServer(app).listen(port, () => {
-  console.log('App server running at http://' + os.hostname() + ':' + port);
+http.createServer(app).listen(process.env.port, () => {
+  console.log(
+    "App server running at http://" + os.hostname() + ":" + process.env.port
+  );
 
   // Enables auto registration/de-registration of app into a host in dev mode
   if (devEnv) addon.register();
